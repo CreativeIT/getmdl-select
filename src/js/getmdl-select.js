d0b361014b4f6fd94c1f59f3fdfb5ccdeed5aa44
Denis <sinedsem@gmail.com>
2016-07-25 01:15:04 +0300

    Narrowed scope for upgradeDom command. Added an option to show select list with up and down arrows.

--- src/js/getmdl-select.js
+++ src/js/getmdl-select.js
@@ -3,7 +3,9 @@
     window.onload = function () {
         getmdlSelect.init('.getmdl-select');
         document.addEventListener("DOMNodeInserted", function (ev) {
-            componentHandler.upgradeDom();
+            if (ev.relatedNode.querySelectorAll(".getmdl-select").length > 0) {
+                componentHandler.upgradeDom();
+            }
         }, false);
     };
 
@@ -11,15 +13,25 @@
         addEventListeners: function (dropdown) {
             var input = dropdown.querySelector('input');
             var list = dropdown.querySelectorAll('li');
+            var menu = dropdown.querySelector('.mdl-js-menu');
+
+            //show menu on mouse down or mouse up
+            input.onkeydown = function (event) {
+                if (event.keyCode == 38 || event.keyCode == 40) {
+                    menu['MaterialMenu'].show();
+                }
+            };
+
+            //return focus to input
+            menu.onkeydown = function (event) {
+                if (event.keyCode == 13) {
+                    input.focus();
+                }
+            };
 
             [].forEach.call(list, function (li) {
                 li.onclick = function () {
-
-                    dropdown.MaterialTextfield.change(li.textContent); // handles css class changes
-                    
-                    // update input with the "id" value
-                    input.dataset.val = li.dataset.val || '';
-
+                    input.value = li.textContent;
                     if ("createEvent" in document) {
                         var evt = document.createEvent("HTMLEvents");
                         evt.initEvent("change", false, true);
@@ -27,7 +39,7 @@
                     } else {
                         input.fireEvent("onchange");
                     }
-                }
+                };
             });
         },
         init: function (selector) {
