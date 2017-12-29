dda087e37dd2f2a9aecea3e41b05e59f5e2f0d6a
Alex <alexbananabob@gmail.com>
2017-12-29 15:59:10 +0300

    Version 2.0

--- README.md
+++ README.md
@@ -8,67 +8,88 @@ Material Design Lite selectfield component [material-design-lite](https://github
 
 Check out the [example](http://creativeit.github.io/getmdl-select/)
 
+## Install
+
+There are three ways to install getmdl-select:
+
+ 1. Using [npm](http://npmjs.org/):
+    Use this command in your command line: 
+    ```bash
+    npm install getmdl-select
+    ```
+ 2. Using [Bower](http://bower.io/):
+    Use this command in your command line: 
+    ```bash
+    bower install getmdl-select
+    ```
+ 3. Clone the repo using Git:
+
+    ```bash
+    git clone https://github.com/CreativeIT/getmdl-select.git
+    ```
+    
+    (Alternatively you can [download](https://github.com/CreativeIT/getmdl-select/archive/master.zip)
+this repository.)
+    And then:
+    
+    ```bash
+    cd getmdl-select   
+    npm install
+    ```
+
 ## Basic use
 To use any MDL component, you must include the minified CSS and JavaScript files using standard relative-path references in the `<head>` section of the page, as described in the MDL Introduction.
    ```html
    <!-- getmdl -->
    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-   <link rel="stylesheet" href="https://code.getmdl.io/1.1.1/material.indigo-pink.min.css">
-   <script defer src="https://code.getmdl.io/1.1.1/material.min.js"></script>   
-   <!--getmdl-select-->
-   <script defer src="getmdl-select.min.js"></script>
-   <link rel="stylesheet" href="getmdl-select.min.css">
+   <link rel="stylesheet" href="your_path_to/material-design-lite/material.min.css">
+   <script defer src="your_path_to/material-design-lite/material.min.js"></script>   
+   <!--getmdl-select-->   
+   <link rel="stylesheet" href="path_to/getmdl-select/getmdl-select.min.css">
+   <script defer src="path_to/getmdl-select/getmdl-select.min.js"></script>
    ```
 
 ### Example
 
-Select field.
+Simple select field. To see other examples visit [this page](http://creativeit.github.io/getmdl-select/).
  ```html
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select">
-      <input class="mdl-textfield__input" value="Belarus" type="text" id="country" readonly tabIndex="-1" data-val="BLR"/>
-        <label class="mdl-textfield__label" for="country">Country</label>
-        <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu" for="country">
-          <li class="mdl-menu__item" data-val="BLR">Belarus</li>
-          <li class="mdl-menu__item" data-val="RUS">Russia</li>
-        </ul>
+    <div class="mdl-textfield mdl-js-textfield getmdl-select">
+       <input class="mdl-textfield__input" value="" id="country" readonly/>
+       <input value="" type="hidden" name="country"/>
+       <label class="mdl-textfield__label" for="country">Сountry</label>
+       <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu" for="country">
+         <li class="mdl-menu__item" data-val="BLR">Belarus</li>
+         <li class="mdl-menu__item" data-val="BRA">Brazil</li>
+         <li class="mdl-menu__item" data-val="FRA">France</li>
+         <li class="mdl-menu__item" data-val="DEU">Germany</li>
+         <li class="mdl-menu__item" data-val="RUS">Russia</li>
+       </ul>
     </div>
  ```
+### Important
+If you want to use more than one getmdl-select item use different ids for inputs and don't forget to change `ul` and `label`'s `for` attributes. It works wrong without this.
 
-
-## Additional 
+## Configuration options 
 #### Dynamically usage
-For dynamically usage, you must add `getmdlSelect.init(cssSelector)` in javascript code, (where cssSelector, for example, is `".getmdl-select"` or `"#mySelect"`), after new item is created. 
+For dynamically usage, you must add `getmdlSelect.init(cssSelector)` in javascript code, (where cssSelector, for example, is `".getmdl-select"` or `"#mySelect"`), after new item is created or any new element added to existing list. 
+
+#### data-val
+Every `li` should have its own `data-val` attribute. Choosing any element of select you change `input`'s `data-val` property, which you can get using `document.querySelector(selector).getAttribute('data-val');`, where selector is your `input`'s id.
+
+#### Pre-selected item
+To set pre-selected value add `data-selected="true"` attribute to corresponding `li` in your list.
 
 #### Width
-Select automatically adapt to the maximum width of values. If you want to use the default width, add class `getmdl-select__fullwidth`.
+Initial Select takes the default width (300px). You can change it by overriding the CSS property `width`.
 
 #### Height
-Select automatically adapt to the maximum height. If you want to use small height (300px) and see scroll bar, add class `getmdl-select__fix-height`.    
+Options list automatically adapt to the maximum height by content. If you want to use small height (300px) and see scroll bar, add class `getmdl-select__fix-height`.
     
-## Install
-
- * [Bower](http://bower.io/): `bower install getmdl-select`
- * [npm](http://npmjs.org/): `npm install getmdl-select`
- 
-### Download / Clone
-
-Clone the repo using Git:
-
-```bash
-git clone https://github.com/CreativeIT/getmdl-select.git
-```
-
-Alternatively you can [download](https://github.com/CreativeIT/getmdl-select/archive/master.zip)
-this repository.
-
-### Build
-
-To get started modifying the components or the docs, first install the necessary
-dependencies, from the root of the project:
-
-```bash
-npm install 
-```
-
 ## LICENSE
 See the [LICENSE file](https://github.com/CreativeIT/getmdl-select/blob/master/LICENSE.txt) for license rights and limitations (MIT).
+
+## Support the project
+* Star the repo
+* Create issue report or feature request
+* [Tweet about it](https://twitter.com/CreativeITeam)
+* Follow us on [Twitter](https://twitter.com/CreativeITeam)
--- src/js/getmdl-select.js
+++ src/js/getmdl-select.js
@@ -1,60 +1,189 @@
 {
     'use strict';
-    window.onload = function () {
-        getmdlSelect.init('.getmdl-select');
-        document.addEventListener("DOMNodeInserted", function (ev) {
-            if (ev.relatedNode.querySelectorAll(".getmdl-select").length > 0) {
-                componentHandler.upgradeDom();
-            }
-        }, false);
-    };
+    (function () {
+        function whenLoaded() {
+            getmdlSelect.init('.getmdl-select');
+        };
+
+        window.addEventListener ?
+            window.addEventListener("load", whenLoaded, false) :
+            window.attachEvent && window.attachEvent("onload", whenLoaded);
+
+    }());
 
     var getmdlSelect = {
-        addEventListeners: function (dropdown) {
+        _addEventListeners: function (dropdown) {
             var input = dropdown.querySelector('input');
+            var hiddenInput = dropdown.querySelector('input[type="hidden"]');
             var list = dropdown.querySelectorAll('li');
             var menu = dropdown.querySelector('.mdl-js-menu');
+            var arrow = dropdown.querySelector('.mdl-icon-toggle__label');
+            var label = '';
+            var previousValue = '';
+            var previousDataVal = '';
+            var opened = false;
 
-            //show menu on mouse down or mouse up
-            input.onkeydown = function (event) {
-                if (event.keyCode == 38 || event.keyCode == 40) {
+            var setSelectedItem = function (li) {
+                var value = li.textContent.trim();
+                input.value = value;
+                list.forEach(function (li) {
+                    li.classList.remove('selected');
+                });
+                li.classList.add('selected');
+                dropdown.MaterialTextfield.change(value); // handles css class changes
+                setTimeout(function () {
+                    dropdown.MaterialTextfield.updateClasses_(); //update css class
+                }, 250);
+
+                // update input with the "id" value
+                hiddenInput.value = li.dataset.val || '';
+
+                previousValue = input.value;
+                previousDataVal = hiddenInput.value;
+
+                if ("createEvent" in document) {
+                    var evt = document.createEvent("HTMLEvents");
+                    evt.initEvent("change", false, true);
+                    menu['MaterialMenu'].hide();
+                    input.dispatchEvent(evt);
+                } else {
+                    input.fireEvent("onchange");
+                }
+            }
+
+            var hideAllMenus = function () {
+                opened = false;
+                input.value = previousValue;
+                hiddenInput.value = previousDataVal;
+                if (!dropdown.querySelector('.mdl-menu__container').classList.contains('is-visible')) {
+                    dropdown.classList.remove('is-focused');
+                }
+                var menus = document.querySelectorAll('.getmdl-select .mdl-js-menu');
+                [].forEach.call(menus, function (menu) {
+                    menu['MaterialMenu'].hide();
+                });
+                var event = new Event('closeSelect');
+                menu.dispatchEvent(event);
+            };
+            document.body.addEventListener('click', hideAllMenus, false);
+
+            //hide previous select after press TAB
+            dropdown.onkeydown = function (event) {
+                if (event.keyCode == 9) {
+                    input.value = previousValue;
+                    hiddenInput.value = previousDataVal;
+                    menu['MaterialMenu'].hide();
+                    dropdown.classList.remove('is-focused');
+                }
+            };
+
+            //show select if it have focus
+            input.onfocus = function (e) {
+                menu['MaterialMenu'].show();
+                menu.focus();
+                opened = true;
+            };
+
+            input.onblur = function (e) {
+                e.stopPropagation();
+            };
+
+            //hide all old opened selects and opening just clicked select
+            input.onclick = function (e) {
+                e.stopPropagation();
+                if (!menu.classList.contains('is-visible')) {
                     menu['MaterialMenu'].show();
+                    hideAllMenus();
+                    dropdown.classList.add('is-focused');
+                    opened = true;
+                } else {
+                    menu['MaterialMenu'].hide();
+                    opened = false;
                 }
             };
 
-            //return focus to input
+            input.onkeydown = function (event) {
+                if (event.keyCode == 27) {
+                    input.value = previousValue;
+                    hiddenInput.value = previousDataVal;
+                    menu['MaterialMenu'].hide();
+                    dropdown.MaterialTextfield.onBlur_();
+                    if (label !== '') {
+                        dropdown.querySelector('.mdl-textfield__label').textContent = label;
+                        label = '';
+                    }
+                }
+            };
+
+            menu.addEventListener('closeSelect', function (e) {
+                input.value = previousValue;
+                hiddenInput.value = previousDataVal;
+                dropdown.classList.remove('is-focused');
+                if (label !== '') {
+                    dropdown.querySelector('.mdl-textfield__label').textContent = label;
+                    label = '';
+                }
+            });
+
+            //set previous value and data-val if ESC was pressed
             menu.onkeydown = function (event) {
-                if (event.keyCode == 13) {
-                    input.focus();
+                if (event.keyCode == 27) {
+                    input.value = previousValue;
+                    hiddenInput.value = previousDataVal;
+                    dropdown.classList.remove('is-focused');
+                    if (label !== '') {
+                        dropdown.querySelector('.mdl-textfield__label').textContent = label;
+                        label = '';
+                    }
                 }
             };
-            
-            [].forEach.call(list, function (li) {
-                li.onclick = function () {
-                    input.value = li.textContent;
-                    dropdown.MaterialTextfield.change(li.textContent); // handles css class changes
-                    setTimeout( function() {
-                        dropdown.MaterialTextfield.updateClasses_(); //update css class
-                    }, 250 );
 
-                    // update input with the "id" value
-                    input.dataset.val = li.dataset.val || '';
-
-                    if ("createEvent" in document) {
-                        var evt = document.createEvent("HTMLEvents");
-                        evt.initEvent("change", false, true);
-                        input.dispatchEvent(evt);
+            if (arrow) {
+                arrow.onclick = function (e) {
+                    e.stopPropagation();
+                    if (opened) {
+                        menu['MaterialMenu'].hide();
+                        opened = false;
+                        dropdown.classList.remove('is-focused');
+                        dropdown.MaterialTextfield.onBlur_();
+                        input.value = previousValue;
+                        hiddenInput.value = previousDataVal;
                     } else {
-                        input.fireEvent("onchange");
+                        hideAllMenus();
+                        dropdown.MaterialTextfield.onFocus_();
+                        input.focus();
+                        menu['MaterialMenu'].show();
+                        opened = true;
                     }
                 };
+            }
+
+            [].forEach.call(list, function (li) {
+                li.onfocus = function () {
+                    dropdown.classList.add('is-focused');
+                    var value = li.textContent.trim();
+                    input.value = value;
+                    if (!dropdown.classList.contains('mdl-textfield--floating-label') && label == '') {
+                        label = dropdown.querySelector('.mdl-textfield__label').textContent.trim();
+                        dropdown.querySelector('.mdl-textfield__label').textContent = '';
+                    }
+                };
+
+                li.onclick = function () {
+                    setSelectedItem(li);
+                };
+
+                if (li.dataset.selected) {
+                    setSelectedItem(li);
+                }
             });
         },
         init: function (selector) {
             var dropdowns = document.querySelectorAll(selector);
-            [].forEach.call(dropdowns, function (i) {
-                getmdlSelect.addEventListeners(i);
-                i.style.width = i.querySelector('.mdl-menu').clientWidth + 'px';
+            [].forEach.call(dropdowns, function (dropdown) {
+                getmdlSelect._addEventListeners(dropdown);
+                componentHandler.upgradeElement(dropdown);
+                componentHandler.upgradeElement(dropdown.querySelector('ul'));
             });
         }
     };
